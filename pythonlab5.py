1c6db463c01189d3112165c373cc577a7f27f189
ruze-gz <scottli2000@hotmail.com>
2018-07-25 23:29:28 +0800

    lab5

--- pythonlab5.py
+++ pythonlab5.py
@@ -1,5 +1,4 @@
-#this is a script for switch IOS upgrade
-#Target update include 2960X, 3850X, 9300X
+# this is a practice script for ios pings
 
 import sys
 import paramiko
@@ -21,6 +20,31 @@ for line in iplist.readlines():
         ip = line.strip()
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname=ip,username=username,password=password)
-        
+        ssh_client.connect(hostname=ip,username=username,password=password,look_for_keys=False)
+        print "you have connected to ", ip
+        command = ssh_client.invoke_shell()
+        cmdlist = open (cmd_file, 'r')
+        cmdlist.seek(0)
+        for line in cmdlist.readlines():
+            command.send(line + "\n")
+        time.sleep(2)
+        cmdlist.close()
+        output = command.recv(65535)
+        print output
+    except paramiko.ssh_exception.AuthenticationException:
+        print "User authentication failed for " + ip + "."
+        switch_with_authentication_issue.append(ip)
+    except socket.error:
+        print ip + "is not reachable."
+        switch_not_reachable.append(ip)
 
+    iplist.close()
+    ssh_client.close
+
+    print '\nUser authentication failed for below switches: '
+    for i in switch_with_authentication_issue:
+        print i
+
+    print 'nBelow switches are not reachable: '
+    for i in switch_not_reachable:
+        print i
